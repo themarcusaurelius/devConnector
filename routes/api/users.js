b1ff42de6dc9bf9f9442aa67033b0b85ceee703c
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-29 11:32:17 -0500

    JWT Implemented and Returning Token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check')
 
 const User = require('../../models/User')
@@ -26,7 +28,6 @@ async (req, res) => {
     }
 
     //const {name, email, password} = req.body;
-    
     const name = req.body.name.toString();   
     const password = req.body.password.toString();   
     const email = req.body.email.toLowerCase().toString();
@@ -58,14 +59,27 @@ async (req, res) => {
 
         // Encrypt password
         const salt = await bcrypt.genSalt(10);
-
         user.password = await bcrypt.hash(password, salt);
 
         //Save user to DB
         await user.save();
 
         // Return jsonwebtoken
-        res.send('User Registered');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            { expiresIn: 36000 }, 
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token })
+            }
+        );
 
     } catch(err) {
         console.error(err.message)
