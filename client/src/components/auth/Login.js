3a1e84235fc15ebcf1a4b851a13092f8748b04d7
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-31 18:08:41 -0500

    Login User

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
     const [formData, setFormData] = useState({
         email: '',
         password: '',
@@ -14,13 +17,17 @@ const Login = () => {
 
     const onSubmit = async e => {
         e.preventDefault();
-        
-        console.log('SUCCESS')
+        login(email, password);
     };
+
+    // Redirect if logged in
+    if (isAuthenticated) {
+        return <Redirect to="/dashboard" />
+    } 
 
     return (
         <Fragment>
-            <h1 className="large text-primary">Login</h1>
+            <h1 className="large text-primary">Sign In</h1>
             <p className="lead">
                 <i className="fas fa-user" /> Sign Into Your Account
             </p>
@@ -59,4 +66,13 @@ const Login = () => {
     );
 }
 
-export default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+    isAuthenticated: PropTypes.bool,
+}
+
+const mapStateToProps = state => ({
+    isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,9 @@ import {
     REGISTER_SUCCESS, 
     REGISTER_FAIL,
     USER_LOADED,
-    AUTH_ERROR
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL
 } from '../actions/types';
 
 const initialState = {
@@ -23,7 +25,8 @@ export default function(state = initialState, action) {
                 loading: false,
                 user: payload
             }
-        case REGISTER_SUCCESS: 
+        case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:
             localStorage.setItem('token', payload.token);
             return {
                 ...state, 
@@ -34,12 +37,13 @@ export default function(state = initialState, action) {
             }
         case REGISTER_FAIL:
         case AUTH_ERROR:
+        case LOGIN_FAIL:
             localStorage.removeItem('token');
             return{
                 ...state, 
                 token: null, 
                 isAuthenticated: false,
-                loading: true
+                loading: false
             }
         default: 
             return state;
