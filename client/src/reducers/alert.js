614a2d0c13002048f2013f5ad31bacc14ccc888a
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-31 12:31:31 -0500

    Alert Component and Action Call Setup With Redux/Devtools

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,11 +1,11 @@
 import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
 const initialState = [
-    {
-        id: 1,
-        msg: 'Please log in',
-        alertType: 'success'
-    }
+    // {
+    //     id: 1,
+    //     msg: 'Please log in',
+    //     alertType: 'success'
+    // }
 ]
 
 export default function(state = initialState, action) {
