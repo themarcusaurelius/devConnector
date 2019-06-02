f3b0fd33b34453ef79cf4fd1b252a79e0b177ad8
Marcus Aurelius <markmayfield16@gmail.com>
2019-06-02 10:59:51 -0500

    Added Profile Reducer & Get Current Profile

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layouts/Alert';
 import Dashboard from './components/dashboard/Dashboard';
-import PrivateRoute from './components/routing/PrivateRoute'
+import PrivateRoute from './components/routing/PrivateRoute';
 import { loadUser } from './actions//auth';
 import setAuthToken from './utils/authToken';
 
