a21cc169b73d713ba197b639bc88067e8174a071
Marcus Aurelius <markmayfield16@gmail.com>
2019-06-03 11:42:32 -0500

    Add Education and Experiences Forms

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,11 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layouts/Alert';
 import Dashboard from './components/dashboard/Dashboard';
-import CreateProfile from './components/profile-form/CreateProfile';
-import EditProfile from './components/profile-form/EditProfile';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
-
 
 // Redux
 import { Provider } from 'react-redux';
@@ -43,6 +44,8 @@ const App = () => {
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+              <PrivateRoute exact path="/add-experience" component={AddExperience} />
+              <PrivateRoute exact path="/add-education" component={AddEducation} />
             </Switch>
           </section>
         </Fragment>
