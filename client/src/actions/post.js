9824a898f68b1e516a83b67f5dfad17f0bc57d81
Marcus Aurelius <markmayfield16@gmail.com>
2019-06-05 12:45:22 -0500

    Single Post Display

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,7 +5,8 @@ import {
     POST_ERROR,
     UPDATE_LIKES,
     DELETE_POST,
-    ADD_POST
+    ADD_POST,
+    GET_POST
 } from './types';
 
 // Get Posts
@@ -78,7 +79,7 @@ export const deletePost = id => async dispatch => {
     }
 };
 
-// Delete Post
+// Add Post
 export const addPost = formData => async dispatch => {
     const config = {
         headers: {
@@ -95,6 +96,23 @@ export const addPost = formData => async dispatch => {
         });
 
         dispatch(setAlert('Post Created', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });
+    }
+};
+
+// Get Post
+export const getPost = id => async dispatch => {
+    try {
+        const res = await axios.get(`/api/posts/${id}`);   
+        
+        dispatch({
+            type: GET_POST,
+            payload: res.data
+        });
     } catch (err) {
         dispatch({
             type: POST_ERROR,
